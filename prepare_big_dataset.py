0698b963183bdcb092317ac75b9f3c4db481440d
Googlefan <google@neody.dev>
2024-09-13 13:13:10 +0000

    fix: prepare

--- prepare_big_dataset.py
+++ prepare_big_dataset.py
@@ -1,4 +1,4 @@
-from datasets import load_dataset
+from datasets import load_dataset, VerificationMode
 from io import BytesIO
 from model.gemma_omni import GemmaOmni
 import librosa
@@ -7,10 +7,24 @@ from datasets import load_dataset
 import torch
 import urllib
 from tqdm import tqdm
+import re
+
+RE = re.compile(r"[a-zA-Z0-9>:]")
+
+
+def is_ok(t, e=True):
+    return not RE.match(t) and len(t) < 150 and (len(t) > 20 if e else True)
 
 
 def collate(model: GemmaOmni):
-    b = load_dataset("HachiML/Hachi-Alpaca", split="v1.0_cleaned")
+    b = load_dataset(
+        "Aruno/guanaco_jp", split="train", verification_mode=VerificationMode.NO_CHECKS
+    )
+    b = b.filter(
+        lambda x: is_ok(x["input"], False)
+        and is_ok(x["instruction"])
+        and is_ok(x["output"])
+    )
     for i, e in enumerate(tqdm(b["instruction"])):
         chat = [
             {
