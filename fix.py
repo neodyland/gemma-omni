319ff33d36e6f9227ace4df50b9e859afd15af10
Googlefan <google@neody.dev>
2024-09-17 01:27:24 +0000

    fix: chat template

--- fix.py
+++ fix.py
@@ -9,14 +9,7 @@ tokenizer: Qwen2TokenizerFast = Qwen2TokenizerFast.from_pretrained(
     "unsloth/Qwen2-1.5B-bnb-4bit"
 )
 snac = SnacGasi()
-tokenizer.chat_template = """
-{% for message in messages %}
-    {{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}
-{% endfor %}
-{% if add_generation_prompt %}
-    {{ '<|im_start|>audio\n' }}
-{% endif %}
-"""
+tokenizer.chat_template = "{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>audio\n' }}{% endif %}"
 tokenizer.add_tokens([f"<audio_token_{i + 1}>" for i in range(snac.size)])
 llm.resize_token_embeddings(len(tokenizer))
 llm.save_pretrained("./data/llm")
