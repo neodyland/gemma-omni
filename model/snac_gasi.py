81624e4ebe0607798ef8fad3ced39fe179456e10
Googlefan <google@neody.dev>
2024-10-19 07:33:18 +0000

    fix: snac

--- model/snac_gasi.py
+++ model/snac_gasi.py
@@ -7,25 +7,36 @@ class SnacGasi(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         self.model = SNAC.from_pretrained("hubertsiuzdak/snac_24khz")
-        self.size = self.model.codebook_size
+        self.vocab_size = self.model.codebook_size * sum(self.model.vq_strides)
         self.sr = 24000
 
+    @torch.inference_mode()
     def encode(self, ids_bef: torch.Tensor):
         bs = ids_bef.size(0)
-        with torch.inference_mode():
-            ids = self.model.encode(ids_bef)
+        ids = self.model.encode(ids_bef)
         ids_new_all = []
         for b in range(bs):
             ids_new = []
             for i in range(len(ids[0][b])):
                 ids_new.extend(ids[0][b][i : i + 1].tolist())
-                ids_new.extend(ids[1][b][i * 2 : i * 2 + 2].tolist())
-                ids_new.extend(ids[2][b][i * 4 : i * 4 + 4].tolist())
+                ids_new.extend(
+                    [
+                        x + self.model.codebook_size
+                        for x in ids[1][b][i * 2 : i * 2 + 2].tolist()
+                    ]
+                )
+                ids_new.extend(
+                    [
+                        x + self.model.codebook_size * 3
+                        for x in ids[2][b][i * 4 : i * 4 + 4].tolist()
+                    ]
+                )
             ids_new_all.append(ids_new)
         return torch.tensor(ids_new_all).to(ids_bef.device, non_blocking=True)
 
+    @torch.inference_mode()
     def decode(self, ids: torch.Tensor):
-        bs = len(ids)
+        bs = ids.size(0)
         ids_old_all_1 = []
         ids_old_all_2 = []
         ids_old_all_3 = []
@@ -33,36 +44,46 @@ class SnacGasi(nn.Module):
             ids_old_1 = []
             ids_old_2 = []
             ids_old_3 = []
-            for i in range(0, len(ids[b]), 7):
+            for i in range(0, len(ids[b, :]), 7):
                 ids_old_1.extend(
-                    [ids[b][i]],
+                    [ids[b, i]],
                 )
                 ids_old_2.extend(
-                    [ids[b][i + 1], ids[b][i + 2]],
+                    [
+                        ids[b, i + 1] - self.model.codebook_size,
+                        ids[b, i + 2] - self.model.codebook_size,
+                    ],
                 )
                 ids_old_3.extend(
-                    [ids[b][i + 3], ids[b][i + 4], ids[b][i + 5], ids[b][i + 6]],
+                    [
+                        ids[b, i + 3] - self.model.codebook_size * 3,
+                        ids[b, i + 4] - self.model.codebook_size * 3,
+                        ids[b, i + 5] - self.model.codebook_size * 3,
+                        ids[b, i + 6] - self.model.codebook_size * 3,
+                    ],
                 )
             ids_old_all_1.append(ids_old_1)
             ids_old_all_2.append(ids_old_2)
             ids_old_all_3.append(ids_old_3)
-        with torch.inference_mode():
-            return self.model.decode(
-                [
-                    torch.tensor(x).to(ids.device, dtype=ids.dtype, non_blocking=True)
-                    for x in [ids_old_all_1, ids_old_all_2, ids_old_all_3]
-                ]
-            )
+        return self.model.decode(
+            [
+                torch.tensor(x).to(ids.device, dtype=ids.dtype, non_blocking=True)
+                for x in [ids_old_all_1, ids_old_all_2, ids_old_all_3]
+            ]
+        )
 
 
 if __name__ == "__main__":
     import librosa
+    import torchaudio
 
     snac = SnacGasi()
-    print(
-        snac.encode(
-            torch.from_numpy(librosa.load("./data/sample.wav", sr=24000)[0])
-            .unsqueeze(0)
-            .unsqueeze(0)
-        ).shape
+    enc = snac.encode(
+        torch.from_numpy(
+            librosa.load("./data/sample.wav", sr=snac.model.sampling_rate)[0]
+        )
+        .unsqueeze(0)
+        .unsqueeze(0)
     )
+    dec = snac.decode(enc).cpu().squeeze(0)
+    torchaudio.save("./data/output.wav", dec, snac.sr)
--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ from transformers import Qwen2ForCausalLM, Qwen2TokenizerFast, BitsAndBytesConfi
 from model.snac_gasi import SnacGasi
 
 llm: Qwen2ForCausalLM = Qwen2ForCausalLM.from_pretrained(
-    "Qwen/Qwen2.5-1.5B-Instruct",
+    "Qwen/Qwen2.5-0.5B-Instruct",
     torch_dtype=torch.bfloat16,
     quantization_config=BitsAndBytesConfig(
         load_in_4bit=True,
@@ -13,11 +13,11 @@ llm: Qwen2ForCausalLM = Qwen2ForCausalLM.from_pretrained(
     ),
 )
 tokenizer: Qwen2TokenizerFast = Qwen2TokenizerFast.from_pretrained(
-    "Qwen/Qwen2.5-1.5B-Instruct"
+    "Qwen/Qwen2.5-0.5B-Instruct"
 )
 snac = SnacGasi()
 tokenizer.chat_template = "{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant_speech\n' }}{% endif %}"
-tokenizer.add_tokens([f"<audio_token_{i + 1}>" for i in range(snac.size)])
+tokenizer.add_tokens([f"<audio_token_{i + 1}>" for i in range(snac.vocab_size)])
 llm.resize_token_embeddings(len(tokenizer))
 llm.save_pretrained("./data/llm")
 tokenizer.save_pretrained("./data/llm")
